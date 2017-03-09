2c16672ebcd71499588ad57ea6ae50651f08c4c6
ali.sabunwala <ali.sabunwala@syscraftonline.com>
2017-03-09 15:01:40 +0530

    Last commit

---
+++ angular/app/modules/dashboard/controllers/section.js
@@ -0,0 +1,238 @@
+dashboard.controller("SectionController", function($rootScope, $scope, apiService, $http, $interval, $location) { //
+    $scope.sectionForm = {};
+    $scope.UserID = '';
+    $scope.Password = '';
+    $scope.ConfPassword = '';
+    $scope.Name = '';
+
+
+    //$scope.UserType = 2;
+
+    
+
+    $scope.AssignedSection = '';
+    $scope.ResetPassword = false;
+    $scope.UserObjectID = '';
+   
+    $scope.button_name = 'Add';
+    $scope.ListInitialized = false;
+    $scope.FormInitialized = true;
+
+    if (!$rootScope.loggedInUser) {
+        apiService.checkloginUser().then(function(res) {
+            if (typeof(res.data.user) != undefined && res.data.user) {
+                $rootScope.loggedInUser = res.data.user;
+            } else {
+                $location.path("/");
+            }
+        });
+    }
+
+    $scope.connection = true;
+    $scope.connection_msg = false;
+    $interval(function() {
+        if (Offline.state == 'down') {
+            $scope.connection = false;
+        } else {
+            if (!$scope.connection) {
+                $scope.connection = true;
+                $scope.connection_msg = true;
+            }
+        }
+    }, 2000)
+
+    $scope.$watchCollection('[connection_msg]', function() {
+        if ($scope.connection_msg) {
+            setTimeout(function() {
+                $scope.connection_msg = false;
+            }, 2000);
+        }
+    });
+
+
+
+    var vm = this;
+
+    $scope.userCurrentPage = 1;
+    $scope.userPageSize = 10;
+    $scope.pageChangeHandler2 = function(num) {
+        console.log('going to page ' + num);
+    };
+
+    $scope.getAllUsers = function() {
+        $scope.ListInitialized = false;
+        apiService.userData().then(function(res) {
+            if (!res.data.IsSuccess) {
+                alert(res.data.message);
+                return;
+            }
+            $scope.userData = res.data.data;
+            $scope.O = 'Name';
+            $scope.userCurrentPage = 1;
+            $scope.userPageSize = 10;
+            $scope.resetControls();
+            $scope.ListInitialized = true;
+        });
+    }
+
+    $scope.getAllUsers();
+
+    apiService.storeData().then(function(res) {
+        $scope.storeData = res.data.data;
+    });
+
+  
+
+    
+
+    $scope.$watchCollection('[AssignedStore]', function() {
+        if ($scope.FormInitialized) {
+            apiService.beaconData($scope.AssignedStore).then(function(res) {
+                $scope.beaconData = res.data.data;
+            });
+        }
+    });
+
+
+    $scope.resetControls = function() {
+        $scope.SectionName = '';
+        $scope.SectionDesc = '';
+         
+         $scope.selectedBeacon = '';
+         
+         $scope.AssignedStore = '';
+
+        $scope.UserObjectID = '';
+        $scope.button_name = "Add";
+        $scope.ResetPassword = false;
+        $scope.sectionForm.$setUntouched();
+        $scope.sectionForm.$setDirty();
+        $scope.sectionForm.$setPristine();
+    }
+
+    $scope.getUser = function(pUserObjectID) {
+        $scope.FormInitialized = false;
+        apiService.getUser(pUserObjectID).
+        success(function(data, status, headers, config) {
+                if (data.data) {
+                    $scope.button_name = 'Update';
+
+                    $scope.UserObjectID = pUserObjectID;
+                    $scope.UserID = data.data[0].UserID;
+                    $scope.Name = data.data[0].Name;
+
+
+                    // $scope.UserType = data.data[0].UserType;
+                    $scope.AssignedStore = data.data[0].AssignedStore;
+                    $scope.AssignedSection = data.data[0].AssignedSection;
+                } else {
+                    $scope.UserObjectID = '';
+                    alert('User not found. Please refresh your page');
+                    $scope.button_name = 'add';
+                }
+                $scope.FormInitialized = true;
+            })
+            .error(function(data, status, headers, config) {
+                console.log("failed.");
+                $scope.FormInitialized = true;
+                return '';
+            });
+    }
+
+
+    $scope.processUser = function() {
+
+        alert($scope.button_name);
+
+        $scope.FormInitialized = false;
+        if ($scope.button_name == 'Add') {
+            alert($scope.button_name);
+ apiService.addSection($scope.SectionName,$scope.SectionDesc,$scope.selectedBeacon,$scope.AssignedStore)
+        .success(function(data, status, headers, config) {
+            
+
+                    if (data.IsSuccess) {
+                        alert('Section Added Successfully');
+                        $scope.getAllUsers();
+                    } else {
+                        alert(data.message);
+                    }
+                    $scope.FormInitialized = true;
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("failed.");
+                    alert(data);
+                    $scope.FormInitialized = true;
+                    return '';
+                });
+        } else {
+
+            $scope.FormInitialized = false;
+            console.log($scope.SectionName);
+            apiService.updateUser($scope.UserObjectID, $scope.UserID, ($scope.SectionName,$scope.SectionDesc,$scope.selectedBeacon,$scope.AssignedStore))
+                .success(function(data, status, headers, config) {
+                    alert('hello');
+                    if (data.IsSuccess) {
+                        alert('User Updated Successfully');
+                        $scope.getAllUsers();
+                    } else {
+                        alert(data.message);
+                    }
+                    $scope.FormInitialized = true;
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("failed.");
+                    $scope.FormInitialized = true;
+                    return '';
+                });
+        }
+
+
+        console.log($scope.button_name);
+        if ($scope.sectionForm.$invalid) {
+            angular.forEach($scope.sectionForm.$error, function(field) {
+                angular.forEach(field, function(errorField) {
+                    errorField.$setTouched();
+                })
+            });
+            //alert("Please check all values on Form.");
+            return;
+        }
+
+        /*  if ($scope.button_name == 'Update') {
+              if ($scope.Password != $scope.ConfPassword) {
+                  alert('Confirm password doesnot match with password');
+                  return;
+              }
+          } else {
+              if (!$scope.Password && !$scope.ConfPassword) {
+                  alert('Password should not be empty.');
+                  return;
+              } else if ($scope.Password != $scope.ConfPassword) {
+                  alert('Confirm password doesnot match with password');
+                  return;
+              }
+          }*/
+
+
+    }
+
+    $scope.deleteUser = function() {
+        var r = confirm("Are you sure to delete this record ?");
+        if (!r) {
+            return;
+        }
+        $scope.FormInitialized = false;
+        apiService.deleteUser($scope.UserObjectID).success(function(res) {
+            console.log(res);
+            $scope.FormInitialized = true;
+            if (res.IsSuccess) {
+                alert('Employee Deleted Successfully');
+                $scope.getAllUsers();
+            } else {
+                alert(res.message);
+            }
+        });
+    }
+
+})
