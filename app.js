fe657033b33f76b41810cd41e8d6f0c185a7808b
manish-syscraft <ali.sabunwala@syscraftonline.com>
2017-09-07 20:45:55 +0530

    live URL Implemented

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ var devicecron = require('node-cron');
 //var mongourl = 'mongodb://lotus:remote@ds161255.mlab.com:61255/lotusbeacon'; //Live Database Ali Account
 var mongourl = 'mongodb://lotus:remote@ds145071-a0.mlab.com:45071,ds145071-a1.mlab.com:45071/lotusbeaconlive?replicaSet=rs-ds145071'; //Live Database Syscraft Info Account paid cluster
 
-//var lotusWebURL = 'https://www.lotuselectronics.com/v2/';
-var lotusWebURL = 'https://www.lotuselectronics.com/lotus2017/demo/v2/';
+var lotusWebURL = 'https://www.lotuselectronics.com/v2/';
+//var lotusWebURL = 'https://www.lotuselectronics.com/lotus2017/demo/v2/';
 
 //var lotusWebURL = 'http://lampdemos.com/lotus15/v2/';
 var lotusURL = 'https://www.lotuselectronics.com/v2_emp/';
